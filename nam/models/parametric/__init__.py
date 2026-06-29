9e442b765bfff0ce5de8de7d9e684a67a932e231
Phillip Self <phillipmself@gmail.com>
2026-06-28 19:52:01 -0500

    Add ConcatLSTM
    Add initial ConcatLSTM model that will be used by an ensemble of models
    for active learning later.

--- nam/models/parametric/__init__.py
+++ nam/models/parametric/__init__.py
@@ -14,14 +14,17 @@ from ._export import bake
 from ._export import bake_to_files
 from ._export import export_parametric
 from ._export import output_scale_from_datasets
+from ._concat_lstm import ConcatLSTM
 from ._hypernet import Hypernetwork
 from ._hyperwavenet import HyperWaveNet
 from ._spec import ParamSpec
 
 _register_dataset_initializer("parametric", _init_dataset)
+_factory.register("ConcatLSTM", ConcatLSTM.init_from_config)
 _factory.register("HyperWaveNet", HyperWaveNet.init_from_config)
 
 __all__ = [
+    "ConcatLSTM",
     "Hypernetwork",
     "HyperWaveNet",
     "ParamSpec",
